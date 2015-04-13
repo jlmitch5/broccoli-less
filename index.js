4bd26612393230efb1f14f3feb994b9a5e9963d0
Joe Fiorini <joe@joefiorini.com>
2015-04-13 13:32:40 -0400

    Allow whitelisting files to include in less output

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var Filter = require('broccoli-filter');
 var RSVP = require('rsvp');
 var less = require('less');
+var Minimatch = require('minimatch').Minimatch;
 
 function LessFilter(inputTree, options) {
 	if (!(this instanceof LessFilter)) {
@@ -9,6 +10,8 @@ function LessFilter(inputTree, options) {
 	}
 
 	this.inputTree = inputTree;
+  this.includeFiles = options.include || [];
+  delete options.include;
 	this.options = options || {};
 }
 
@@ -17,6 +20,31 @@ LessFilter.prototype.constructor = LessFilter;
 
 LessFilter.prototype.extensions = ['less'];
 LessFilter.prototype.targetExtension = 'css';
+
+var canProcessFile = LessFilter.prototype.canProcessFile;
+
+LessFilter.prototype.canProcessFile = function(relativePath) {
+
+    // If not using include, then default to previous behavior of processing
+    // everything
+    //
+    if (this.includeFiles == []) {
+        return canProcessFile.apply(this, [relativePath]);
+    }
+
+    var includedFiles =
+        this.includeFiles.filter(function(pattern) {
+            if (this._includeFile(relativePath, new Minimatch(pattern))) {
+                return true;
+            }
+        }.bind(this));
+
+    return includedFiles.length > 0;
+};
+
+LessFilter.prototype._includeFile = function(relativePath, pattern) {
+    return pattern.match(relativePath);
+}
 
 LessFilter.prototype.processString = function (str, relativePath) {
 
