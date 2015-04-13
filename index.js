19096ce98ebe45e2fbbffaa535462978f1cf91ee
Joe Fiorini <joe@joefiorini.com>
2015-04-13 14:16:33 -0400

    Use absolute path for includePaths option

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var Filter = require('broccoli-filter');
 var RSVP = require('rsvp');
 var less = require('less');
 var Minimatch = require('minimatch').Minimatch;
+var fs = require('fs');
+var path = require('path');
 
 function LessFilter(inputTree, options) {
 	if (!(this instanceof LessFilter)) {
@@ -46,9 +48,25 @@ LessFilter.prototype._includeFile = function(relativePath, pattern) {
     return pattern.match(relativePath);
 }
 
-LessFilter.prototype.processString = function (str, relativePath) {
+
+LessFilter.prototype.processFile = function (srcDir, destDir, relativePath) {
+  var self = this
+  var inputEncoding = (this.inputEncoding === undefined) ? 'utf8' : this.inputEncoding
+  var outputEncoding = (this.outputEncoding === undefined) ? 'utf8' : this.outputEncoding
+  var string = fs.readFileSync(srcDir + '/' + relativePath, { encoding: inputEncoding })
+  return RSVP.Promise.resolve(this.processString(string, srcDir, relativePath))
+    .then(function (outputString) {
+      var outputPath = self.getDestFilePath(relativePath)
+      fs.writeFileSync(destDir + '/' + outputPath, outputString, { encoding: outputEncoding })
+    })
+}
+
+LessFilter.prototype.processString = function (str, srcDir, relativePath) {
 
     this.options.filename = this.options.filename || relativePath;
+    this.options.paths = this.options.paths.map(function(relative) {
+        return path.join(srcDir, relative);
+    });
 
     return less.render(str, this.options).then(function(output) {
         return output.css;
