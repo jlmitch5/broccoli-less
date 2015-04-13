50e60a091da6090afa69eb99d69c3128a77f5110
Joe Fiorini <joe@joefiorini.com>
2015-04-13 13:13:58 -0400

    Use promise-form of less call and improve error reporting

--- index.js
+++ index.js
@@ -18,17 +18,18 @@ LessFilter.prototype.constructor = LessFilter;
 LessFilter.prototype.extensions = ['less'];
 LessFilter.prototype.targetExtension = 'css';
 
-LessFilter.prototype.processString = function (str) {
-	return new RSVP.Promise(function(resolve, reject) {
-		less.render(str, this.options, function (err, data) {
-			if (err) {
-				reject(err);
-				return;
-			}
+LessFilter.prototype.processString = function (str, relativePath) {
 
-			resolve(data.css);
-		});
-	}.bind(this));
+    this.options.filename = this.options.filename || relativePath;
+
+    return less.render(str, this.options).then(function(output) {
+        return output.css;
+    }).catch(function(error) {
+        console.error("Error processing file ", error.filename, "at line ", error.line);
+        console.error(error.message);
+        console.error("Near:");
+        console.error(error.extract.join('\n'));
+    });
 };
 
 module.exports = LessFilter;
